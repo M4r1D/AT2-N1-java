0acab860cdcab27fed6f4625a04ae2b0795ce567
Mari <122611455+M4r1D@users.noreply.github.com>
2024-04-27 00:42:35 -0300

    Add files via upload

--- Cliente.java
+++ Cliente.java
@@ -7,8 +7,9 @@ realizar compras na loja 1 e loja 2
 */
 package AT2_N1;
 
-public class Cliente{
+public class Cliente extends Thread{
 	private String nome;
+	private double saldo;
 	private String num_conta;
 	
 	//construtores
@@ -22,6 +23,11 @@ public class Cliente{
 	public Cliente(String nome,String num_conta) {
 		this.nome=nome;
 		this.num_conta=num_conta;
+	}
+	public Cliente(String nome,String num_conta,double saldo) {
+		this.nome=nome;
+		this.num_conta=num_conta;
+		this.saldo=saldo;
 	}
 	
 	//get set
@@ -37,8 +43,35 @@ public class Cliente{
 	public void SetNum_conta(String num_conta) {
 		this.num_conta=num_conta;
 	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
 	
-	//métodos - realizar compras loja1 e 2
 	
+	//métodos - realizar compras loja1  
+	void exibeSaldo() {
+		System.out.println(this.nome + "seu saldo é R$"+ this.saldo);
+	}
+	
+	void Compras() {
+		int count = 5;
+		int i = 0;
+		while(i < count) {
+			if(this.saldo>200) {
+				this.saldo -=200;
+				i++;
+				System.out.println(this.nome + "seu saldo é R$"+ this.saldo);
+			}
+			else {
+				System.out.println("Saldo indisponível!");
+				break;
+			}
+		}
+	}
 	
 }
